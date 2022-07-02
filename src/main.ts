23a608fb84a7fc54bcc321bb895c9acf52dddc1e
ChaMyungjun <acg70159@gmail.com>
2022-07-02 23:41:54 +0900

    [UPDATE] Update cors enable

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from '@/src/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors();
   app.setGlobalPrefix('api');
 
   await app.listen(8000);
